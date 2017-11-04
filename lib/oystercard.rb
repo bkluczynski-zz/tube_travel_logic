f06da91148817fd636d4f3a29a78dfdf42c79535
Bart <kluczynskib@gmail.com>
2017-11-04 20:59:14 +0000

    did some refactoring around balance checks

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,3 +1,5 @@
+Maximum_value = 90;
+
 class Oystercard
 
   attr_accessor :balance
@@ -11,7 +13,14 @@ class Oystercard
   end
 
   def top_up(topup)
+    fail "maximum top-up value of $#{Maximum_value} has been reached" unless balance_exceeded?(topup)
     self.balance += topup
+  end
+
+  private
+
+  def balance_exceeded?(topup)
+    self.balance + topup < Maximum_value
   end
 
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,9 +6,17 @@ describe Oystercard do
     expect(subject.get_balance).to eq(0)
   end
 
-  it 'should allow to topup an oyster with a chosen sum of money' do
-    expect { subject.top_up(10) }.to change { subject.get_balance }.from(0).to(10)
-  end
 
+  describe '#top_up' do
+
+    it 'should allow to topup an oyster with a chosen sum of money' do
+      expect { subject.top_up(10) }.to change { subject.get_balance }.from(0).to(10)
+    end
+
+    it 'should refuse to topup an oyster above 90 pounds' do
+      expect { subject.top_up(91) }.to raise_error('maximum top-up value of $90 has been reached')
+    end
+
+  end
 
 end
