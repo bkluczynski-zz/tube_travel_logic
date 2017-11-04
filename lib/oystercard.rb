65547795640a7420063e78020243f7250fb6b172
Bart <kluczynskib@gmail.com>
2017-11-04 19:08:41 +0000

    getting balance on the oyster

---
+++ lib/oystercard.rb
@@ -0,0 +1,14 @@
+class Oystercard
+
+  attr_reader :balance
+
+  def initialize
+    @balance = 0
+  end
+
+  def get_balance
+    balance
+  end
+
+
+end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,11 @@
+require 'oystercard'
+
 describe Oystercard do
+
+  it 'should be initialized with balanace of 0 by default' do
+    expect(subject.get_balance).to eq(0)
+  end
+
 
 
 end
