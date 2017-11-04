390241ebd787407a6eef7539ecb53755d11c9d9b
Bart <kluczynskib@gmail.com>
2017-11-04 21:28:44 +0000

    added in_journey status and touch_in and touch_out methods

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,10 +2,11 @@ MAXIMUM_BALANCE = 90;
 
 class Oystercard
 
-  attr_accessor :balance
+  attr_accessor :balance, :in_journey
 
   def initialize
-    @balance = 0
+    @balance = 0;
+    @in_journey = false;
   end
 
   def get_balance
@@ -19,6 +20,18 @@ class Oystercard
 
   def deduct(amount)
     self.balance -= amount
+  end
+
+  def in_journey?
+    in_journey
+  end
+
+  def touch_in
+    self.in_journey = true
+  end
+
+  def touch_out
+    self.in_journey = false
   end
 
   private
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,9 +2,6 @@ require 'oystercard'
 
 describe Oystercard do
 
-  before(:each) do
-    @maximum_balance = Oystercard::MAXIMUM_BALANCE
-  end
 
   it 'should be initialized with balanace of 0 by default' do
     expect(subject.get_balance).to eq(0)
@@ -12,6 +9,10 @@ describe Oystercard do
 
 
   describe '#top_up' do
+
+    before(:each) do
+      @maximum_balance = Oystercard::MAXIMUM_BALANCE
+    end
 
     it 'should allow to topup an oyster with a chosen sum of money' do
       expect { subject.top_up(10) }.to change { subject.get_balance }.from(0).to(10)
@@ -22,17 +23,30 @@ describe Oystercard do
       expect { subject.top_up(1) }.to raise_error('maximum top-up value of $90 has been reached')
     end
 
-  end
-
-  describe '#deduct' do
-
     it 'should allow to deduct money from an oyster' do
       subject.top_up(@maximum_balance)
       expect { subject.deduct(10) }.to change { subject.get_balance }.from(@maximum_balance).to(80)
-
     end
 
   end
 
+  describe '#in_journey' do
+
+    it 'should have journey? method defaulted to false' do
+      expect(subject.in_journey?).to eq(false)
+    end
+
+    it 'should be able to touch in' do
+      subject.touch_in
+      expect(subject).to be_in_journey
+    end
+
+    it 'should be able to touch_out' do
+      subject.touch_in
+      subject.touch_out
+      expect(subject).not_to be_in_journey
+    end
+
+  end
 
 end
